4cc1c44b18d61b51e98df323b58666a015349370
Balazs Nyari <Nyari-B@ulster.ac.uk>
2025-07-01 17:27:16 +0100

    Navbar implemented

---
+++ app/NavBar.tsx
@@ -0,0 +1,50 @@
+import React from 'react'
+
+import Image from 'next/image'
+import Logo from './tyroneTaxVat.jpg';
+
+import Link from 'next/link'
+
+/* Import router doesnt work */
+
+const NavBar = () => {
+
+  return (
+    <nav>
+<div className="navbar bg-base-100 shadow-sm bg-rose-900">
+  <div className="navbar-start">
+    <p>Mon - Sun : 8:30am - 5pm</p>
+
+  </div>
+  <div className="navbar-center">
+    <a className="btn btn-ghost">
+        
+        <Image 
+        src={Logo}
+        alt= "Logo"
+        width={50}
+        height={50}
+        />
+    </a>
+  </div>
+
+  <div className="navbar-end">
+    <div className="dropdown">
+      <div tabIndex={0} role="button" className="btn btn-ghost btn-circle">
+        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h7" /> </svg>
+      </div>
+      <ul
+        tabIndex={0}
+        className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
+        <li><Link href="/">Homepage</Link></li>
+        <li><a>Portfolio</a></li>
+        <li><a>About</a></li>
+      </ul>
+    </div>
+  </div>
+</div>
+    </nav>
+  )
+}
+
+export default NavBar
--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import Link from 'next/link'
 import Button from './components/ Button';
+import { AiOutlineWhatsApp } from "react-icons/ai";
 
 export default function Home() {
   return (
@@ -8,6 +9,7 @@ export default function Home() {
     <h1 className ='p-5 bg-sky-400'>Hello Joe mama</h1>
     <Link href="/users">Users</Link>
     <Button />
+    <AiOutlineWhatsApp />
   </main>
   );
 }
