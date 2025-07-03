9a19a6762f7b8f1bbd74a73b58c3429b82ae223c
Balazs Nyari <Nyari-B@ulster.ac.uk>
2025-07-03 17:25:23 +0100

    Services Component created

---
+++ app/components/Services.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const services = () => {
+  return (
+    <div className= "flex flex-col items-center gap-6 p-7 md:flex-row md:gap-8 rounded-2xl">
+        <div>
+            <span className= "text-5xl"> Our Services </span>
+        </div>
+        <div className="flex items-center text-rose-900">
+            <span>Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus esse deleniti architecto asperiores explicabo! Porro repellendus ab voluptates ipsa ea quis nihil labore dolores! Atque numquam laboriosam ea in voluptas!</span>
+            <span>Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita veniam laboriosam ipsum accusamus vel. Est tenetur quidem cupiditate voluptatem qui odit quod, facilis iste porro vel sequi quis itaque possimus.</span>
+        </div>
+    </div>
+  )
+}
+
+export default services
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 
 import Hero from './components/Hero';
+import Services from './components/Services';
 
 /*import { AiOutlineWhatsApp } from "react-icons/ai";*/
 
@@ -8,6 +9,7 @@ export default function Home() {
   return (
    <main>
     <Hero />
+    <Services />
 
   </main>
   );
