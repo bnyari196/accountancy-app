5a3fb1921da15e9975614a04bb61a20a0e3f8b5e
Balazs Nyari <Nyari-B@ulster.ac.uk>
2025-07-03 15:40:44 +0100

    Hero implemented

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -6,18 +6,17 @@ const Hero = () => {
   className="hero min-h-screen"
   style={{
     backgroundImage:
-      "url(https://img.daisyui.com/images/stock/photo-1507358522600-9f71e620c44e.webp)",
+      "url(/Omagh.jpg)",
   }}
 >
   <div className="hero-overlay"></div>
   <div className="hero-content text-neutral-content text-center">
     <div className="max-w-md">
-      <h1 className="mb-5 text-5xl font-bold">Hello there</h1>
+      <h1 className="mb-5 text-5xl font-bold">Tyrone Tax & Vat Specialists</h1>
       <p className="mb-5">
-        Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem
-        quasi. In deleniti eaque aut repudiandae et a id nisi.
+        Tyrone based small, friendly tax and Vat Specialist offering Free Consulations
       </p>
-      <button className="btn btn-primary">Get Started</button>
+      <button className="btn btn-primary">Book a Free Consultation</button>
     </div>
   </div>
 </div>
